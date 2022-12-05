542ecf0c7b23abdab9d411f424d073ebacf70829
PC-22 <shahbaziweb@gmail.com>
2022-12-05 09:50:37 +0330

    update dynamic-form-module

--- src/app/dynamic-form/dynamic-form.module.ts
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from "@angular/core";
-import {FormFieldComponent} from "./form-field/form-field.component";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {DynamicFormComponent} from './dynamic-form/dynamic-form.component';
+import {InputTextComponent} from './input-text/input-text.component';
+import {CheckBoxComponent} from './check-box/check-box.component';
 
 @NgModule({
   declarations: [
-    FormFieldComponent,
     DynamicFormComponent,
+    InputTextComponent,
+    CheckBoxComponent,
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     DynamicFormComponent,
